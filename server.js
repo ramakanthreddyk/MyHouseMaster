db1ccb7ced1dacbc51b4f20fcd3e2293ed66b2be
Venkatesh Nagineni <venkatesh.nagineni@verivox.com>
2020-05-08 20:48:04 +0200

    server files deployed

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/my-house-master'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/my-house-master/index.html'));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
